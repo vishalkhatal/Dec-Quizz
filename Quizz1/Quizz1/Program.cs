7409a9b1af38cb09694b88947fab5ef6414e8eaa
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-01 09:34:30 +0530

    Added sample code

---
+++ Quizz1/Quizz1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample obj = new Sample();
+            Sample.first();
+            obj.Second(10);
+        }
+    }
+    class Sample
+    {
+        public static void first()
+        {
+            Console.WriteLine("the first method");
+
+        }
+        public void Second()
+        {
+            first();
+            Console.WriteLine("the second method");
+
+        }
+        public void Second(int i)
+        {
+            Console.WriteLine(i);
+            Second();
+
+        }
+    }
+}

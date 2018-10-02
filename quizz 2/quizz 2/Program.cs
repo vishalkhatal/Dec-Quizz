78746236d34ce90eac4ef8eab7b09a08f698432a
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-02 10:21:08 +0530

    Added sample code

---
+++ quizz 2/quizz 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizz_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample1 z = new Sample1();
+            z.Add();
+        }
+    }
+    class Sample
+    {
+        public int _index = 5;
+
+        public Sample()
+        {
+            _index = 2;
+        }
+    }
+    class Sample1:Sample
+    {
+       
+        public void Add()
+        {
+            _index += 1;
+            Console.WriteLine(_index);
+        }
+    }
+}

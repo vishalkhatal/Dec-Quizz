087f327f0a540e63247e6d3c12720d228ee72453
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-03 09:24:27 +0530

    Added sample code

---
+++ Quizz 3/Quizz 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList aa = new ArrayList();
+            aa.Add(1);
+            aa.Add('1');
+            aa.Add("1");
+            foreach(int a in aa)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}

1886507970eb3c4dd270ae8784c76737259053d0
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-04 09:23:15 +0530

    Added sample code

---
+++ Quizz4/Quizz4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList aa = new ArrayList();
+            aa.Add(1);
+            aa.Add(2);
+            aa.Add(3);
+            aa.Insert(1, 4);
+            foreach (int a in aa)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}

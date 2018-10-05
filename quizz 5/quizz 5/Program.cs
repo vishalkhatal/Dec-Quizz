6023acdd71dba6b6cdeda2e5d17c219afe6b2849
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-05 10:08:14 +0530

    Added sample code

---
+++ quizz 5/quizz 5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizz_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "VISHAL";
+            string b = "vishal";
+            Console.Write(string.Compare(a, b, true));
+
+        }
+    }
+}

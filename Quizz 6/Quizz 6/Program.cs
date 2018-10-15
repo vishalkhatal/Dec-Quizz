67a1ea741c72a892bc01d3c44e2dd6b63593185b
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-15 11:00:35 +0530

    Added sample code

---
+++ Quizz 6/Quizz 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int i = 3;
+            int j = 2;
+
+            func1(ref i);
+            func2(out j);
+            Console.WriteLine(i + " and " + j);
+        }
+        static void func1(ref int num)
+        {
+            num = num + num;
+        }
+        static void func2(out int num)
+        {
+            num = num * num;
+        }
+    }
+}

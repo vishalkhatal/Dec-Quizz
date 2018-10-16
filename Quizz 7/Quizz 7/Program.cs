2265d9b97d61cd5e09668da00b4ae552ead8c26e
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-16 09:25:30 +0530

    Added sample code

---
+++ Quizz 7/Quizz 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime();
+            if(date==null)
+            {
+                Console.Write("Success");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

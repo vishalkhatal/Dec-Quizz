7f4fa098ae3145e2033cf073a53ee5b5888a0ab6
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-17 10:24:11 +0530

    Added sample code

---
+++ quizz8/quizz8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizz8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] obj = new object[5];
+            obj[0] = "v";
+            obj[1] = 2;
+            Console.ReadKey();
+        }
+    }
+}

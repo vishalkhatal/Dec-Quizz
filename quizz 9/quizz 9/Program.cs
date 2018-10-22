7f930ff1d3660f82583604e9439546c178e50ad2
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-22 09:25:50 +0530

    Added samplec code

---
+++ quizz 9/quizz 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizz_9
+{
+    class Program
+    {
+        static string name;
+        private static string _subject;
+        public static string subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+
+        }
+        static Program()
+        {
+            Console.WriteLine("Static con");
+            name = "static member";
+        }
+        public static void display()
+        {
+            Console.WriteLine("Static method" + name);
+        }
+
+        static void Main(string[] args)
+        {
+            Program.subject = "Csharp";
+            Console.WriteLine("subject" + Program.subject);
+            Program.display();
+        }
+    }
+}

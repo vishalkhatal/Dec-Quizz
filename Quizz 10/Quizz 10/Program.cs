06e2f6f3a990c431c259e72f75e7ab5ba482f189
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-23 09:09:15 +0530

    Added sample code

---
+++ Quizz 10/Quizz 10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num = 100;
+            dynamic val = num;
+            Console.WriteLine(val.GetType());
+            val += 100;
+            Console.WriteLine(val.GetType());
+
+        }
+    }
+}

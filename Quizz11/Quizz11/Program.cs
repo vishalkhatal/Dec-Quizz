9f8605bf3585f2462fcf9d4c75a08537f3f3bace
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-10-24 09:10:32 +0530

    Added sample code

---
+++ Quizz11/Quizz11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            int val = 66;
+            int[] a = new int[5];
+            try
+            {
+                Console.WriteLine("Enter number");
+                index = Convert.ToInt32(Console.ReadLine());
+                a[index] = val;
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured");
+            }
+
+        }
+    }
+}

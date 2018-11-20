796a0255cb59a7d2ca17786c86f567c4db664ff4
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-11-20 09:12:22 +0530

    Added quizz sample

---
+++ Quizz 12/Quizz 12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz_12
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int y = 3;
+            write(out y);
+            return 0;
+        }
+        private static void write(out int y)
+        {
+            Console.WriteLine(y);
+            y = 1;
+        }
+    }
+
+}

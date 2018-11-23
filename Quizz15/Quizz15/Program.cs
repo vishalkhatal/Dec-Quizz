35b82644b84d1b757ce9bfd7e6d7c25d66d72c98
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-11-23 09:25:37 +0530

    Added quizz sample

---
+++ Quizz15/Quizz15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello world !";
+            Console.WriteLine(string.Compare(str, "Hello wrold?").GetType());
+            Console.ReadKey();
+        }
+    }
+}

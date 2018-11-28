7fa94a921bf8c23f1a0d2d1a5d529abf839ac037
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-11-28 09:37:44 +0530

    Added Quizz sample

---
+++ Quizzz 16/Quizzz 16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzz_16
+{
+   //To prove we cant have virtual method in sealed class
+    sealed class Demo
+    {
+        public virtual void Foo()
+        {
+            Console.WriteLine("In Demo");
+        }
+    }
+}

7dc7092ea98eb2485ff515782ce0181622d5a85a
Vishal Khatal <vishal.khatal2206@gmail.com>
2019-01-08 09:21:06 +0530

    Added quizz sample

---
+++ DecQuizz2019_1/DecQuizz2019_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecQuizz2019_1
+{
+    class Program
+    {
+        static string location;
+        static DateTime time;
+        static void Main(string[] args)
+        {
+            Console.Write(location);
+            Console.Write(time.ToString());
+        }
+    }
+}
